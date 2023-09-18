77f0d34d94c4a0d29491df48e32e276d2abb1747
kjfyrt <143996871+kjfyrt@users.noreply.github.com>
2023-09-18 20:49:16 +0300

    Update 14.cpp

--- 14.cpp
+++ 14.cpp
@@ -1,24 +1,6 @@
 #include <iostream>
 #include "easy.h"
 using namespace std;
-int itc_sqrt(int num)
-{
-    if (num < 0){
-        return -1;
-    }
-    if (num == 0){
-        return 0;
-    }
-    if (num > 0){
-        for(long long i = 0; num > i; i++){
-            if (num == i*i){
-            return i;
-        }
-    }
-    return -1;
-
-}
-}
 int itc_str(int a, int b, int c)
 {
     if (a > 0 and b > 0 and c > 0 and a + b > c and a + c > b and b + c > a){
