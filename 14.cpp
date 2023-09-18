27d87548f817a2eb333b8a3e407bf4e8f6003cbc
kjfyrt <143996871+kjfyrt@users.noreply.github.com>
2023-09-18 09:50:03 +0300

    Update 14.cpp

--- 14.cpp
+++ 14.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int itc_str(int a, int b, int c)
 {
-    int p = (a+b+c)/2;
+    int p = (a+b+c)/2.0;
     if (a > 0 and b > 0 and c > 0){
         return sqrt(p*(p-a)*(p-b)*(p-c));
     }
