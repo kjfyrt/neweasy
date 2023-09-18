db95d928ad832092114987473d35dde2b260f1d5
kjfyrt <143996871+kjfyrt@users.noreply.github.com>
2023-09-18 19:26:05 +0300

    Update 14.cpp

--- 14.cpp
+++ 14.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
-#include "easy.h"
-#include <cmath>
+#include "jhg.h"
 using namespace std;
+int itc_sqrt(int num)
+{
+    if (num < 0){
+        return -1;
+    }
+    if (num == 0){
+        return 0;
+    }
+    if (num > 0){
+        for(long long i = 0; num > i; i++){
+            if (num == i*i){
+            return i;
+        }
+    }
+    return -1;
+
+}
+}
 int itc_str(int a, int b, int c)
 {
-    int p = (a+b+c)/2.0;
-    if (a > 0 and b > 0 and c > 0){
-        return sqrt(p*(p-a)*(p-b)*(p-c));
+    if (a > 0 and b > 0 and c > 0 and a + b > c and a + c > b and b + c > a){
+            float p = (a+b+c)/2.0;
+            float sq = itc_sqrt(p*(p-a)*(p-b)*(p-c));
+            return sq;
+
     }
     return -1;
 }
